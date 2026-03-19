51ebf46cf5ca129477904f40714dc9b755e08a69
mmmxxxaaa <maks.katkov.2007@mail.ru>
2026-03-19 13:06:52 +0300

    %b %d %x обрабатываются

--- src/printf.s
+++ src/printf.s
@@ -5,7 +5,7 @@ section .text
 global _start
 
 _start:     mov rdi, test_format
-            mov rsi, '!'
+            mov rsi, 12
             call MyPrintf
 
             mov rax, 0x3C
@@ -71,7 +71,8 @@ ProcessingStack:
             je .common_symbol
 
             call SpecialSymbolProc
-
+            cmp rax, -1
+            je .exit
             inc rdi
             jmp .printing_loop
 .common_symbol:
@@ -85,61 +86,106 @@ ProcessingStack:
 ; -----------------------------------------------------------------------------------------
 ; Обрабатывает спецификатор после символа '%'
 ;
-; Entry: [rdi] = спецификтор
-;        rbp   = аргумент
+; Entry: rdi = адрес в строке, по которому лежит спецификтор
+;        rbp = адрес аргумента в стеке
 ;
 ; Exit:  rbp += 8, если всё хорошо
-;                  если произошла ошибка //FIXME обрабатывать ошибки
-; Destr: rcx
+;        rax = -1  если произошла ошибка //FIXME обрабатывать ошибки
+; Destr: rax, rcx
 ; ----------------------------------------------------------------------------------------
 SpecialSymbolProc:
             cmp byte [rdi], 'a'
-            jb process_invalid
+            jb processInvalid
             cmp byte [rdi], 'z'
-            ja process_invalid
+            ja processInvalid
 
-            xor rcx, rcx          ;!!!
+            xor rcx, rcx           ;!!!
             mov cl, [rdi]
             mov rcx, [jump_table - 8*('a' - rcx)]
             jmp rcx
 return_here_after_jmp_table:
             add rbp, 8
             ret
-process_invalid:                ;//FIXME обрабатывать ошибки
+processInvalid:
+            mov rax, -1                   ;//FIXME обрабатывать ошибки
             ret
 
-
-ProcessBinary:                  ;//TODO
+; ----------------------------------------------------------------------------------------
+; НЕ функция, а просто обработчик вывода символа в двоичной системе счисления
+;
+; Entry: rbp = адрес, по которому лежит число, которое нужно напечатать в двоичной системе счисления
+; Exit:  None
+; Destr:
+; ----------------------------------------------------------------------------------------
+processBinary:
+            push rdi
+            mov rdi, rbp
+            mov rsi, 2
+            call NumberToASCII
+            pop rdi
             jmp return_here_after_jmp_table
 
 ; ----------------------------------------------------------------------------------------
-; НЕ функция, а просто обработчик
+; НЕ функция, а просто обработчик вывода одного символа
 ;
 ; Entry: rbp = адрес, по которому лежит символ, который нужно напечатать
 ; Exit:  None
 ; Destr:
 ; ----------------------------------------------------------------------------------------
-ProcessChar:
+processChar:
             push rdi
             mov rdi, rbp
             call PrintChar
             pop rdi
-
             jmp return_here_after_jmp_table
 
-ProcessDecimal:               ;//TODO
+; ----------------------------------------------------------------------------------------
+; НЕ функция, а просто обработчик вывода символа в десятичной системе счисления
+;
+; Entry: rbp = адрес, по которому лежит число, которое нужно напечатать в десятичной системе счисления
+; Exit:  None
+; Destr:
+; ----------------------------------------------------------------------------------------
+processDecimal:
+            push rdi
+            mov rdi, rbp
+            mov rsi, 10
+            call NumberToASCII
+            pop rdi
             jmp return_here_after_jmp_table
 
-ProcessOct:                   ;//TODO
+; ----------------------------------------------------------------------------------------
+; НЕ функция, а просто обработчик вывода символа в восьмеричной системе счисления
+;
+; Entry: rbp = адрес, по которому лежит число, которое нужно напечатать в восьмеричной системе счисления
+; Exit:  None
+; Destr:
+; ----------------------------------------------------------------------------------------
+processOct: push rdi
+            mov rdi, rbp
+            mov rsi, 8
+            call NumberToASCII
+            pop rdi
             jmp return_here_after_jmp_table
 
-ProcessHex:                   ;//TODO
+; ----------------------------------------------------------------------------------------
+; НЕ функция, а просто обработчик вывода символа в шестнадцатеричной системе счисления
+;
+; Entry: rbp = адрес, по которому лежит число, которое нужно напечатать в шестнадцатеричной системе счисления
+; Exit:  None
+; Destr:
+; ----------------------------------------------------------------------------------------
+processHex: push rdi
+            mov rdi, rbp
+            mov rsi, 16
+            call NumberToASCII
+            pop rdi
             jmp return_here_after_jmp_table
 
-ProcessString:                ;//TODO
+processString:                ;//TODO
             jmp return_here_after_jmp_table
 
-ProcessInvalid:               ;//TODO
+processInvalid:               ;//TODO
             jmp return_here_after_jmp_table
 
 ; ----------------------------------------------------------------------------------------
@@ -151,7 +197,7 @@ ProcessInvalid:               ;//TODO
 ; Destr: ...
 ; ----------------------------------------------------------------------------------------
 PrintChar:
-            push rdi
+            push rdi            ; сохрянем регистры, которые испортим при syscall
             push rsi
             push rdx
             push rax
@@ -169,34 +215,91 @@ PrintChar:
 
             ret
 
+; ----------------------------------------------------------------------------------------
+; Переводит число в набор ASCII кодов для вывода в консоль
+; Entry: rdi = адрес, по которому лежит начало числа
+;        rsi = основание системы счисления
+;
+; Destr:
+; ----------------------------------------------------------------------------------------
+NumberToASCII:
+            push rax            ; используется для хранения значения числа
+            push rbx            ; используется для адресации к буферу
+            push rcx            ; используется для подсчёта количества разрядов
+            push rdx            ; занулим его перед делением (то есть испортим)
+            push rdi            ; испортим его минусом/испортим при syscall
+
+            mov rax, [rdi]      ; в rax число, которое нужно напечатать
+            test rax, rax
+            jns .number_is_positive
+
+.number_is_negative:
+            mov rdi, '-'
+            call PrintChar
+            neg rax
+
+.number_is_positive:
+            mov rbx, num_buffer + num_buffer_size - 1   ;rbx-конец буфера
+            xor rcx, rcx                                ; считаем количество разрядов
+
+.loop:
+            inc rcx
+            xor rdx, rdx        ; div считает делимым большое 128-битное число [rdx][rax]
+            div rsi
+            mov dl, [array_for_converting_numbers + rdx]
+            mov [rbx], dl
+            dec rbx             ; декрементируем, так как идем справа налево по буферу
+            test rax, rax
+            jnz .loop           ; продолжаем до тех пор, пока не получим ноль
+
+            mov rax, 0x01
+            mov rsi, num_buffer + num_buffer_size
+            sub rsi, rcx
+            mov rdi, 1
+            mov rdx, rcx
+            syscall
+
+            pop rdi
+            pop rdx
+            pop rcx
+            pop rbx
+            pop rax
+
+            ret
+
+
 section     .data
 
-test_format db "check = %c", 0xd, 0xa, 0
-test_string db "test string", 0
+
+num_buffer_size equ 64
+num_buffer:  db num_buffer_size dup(0)
+test_format: db "check = %o", 0xd, 0xa, 0
+test_string: db "test string", 0
+array_for_converting_numbers: db "0123456789ABCDEF"
 jump_table:
-            dq ProcessInvalid ; a
-            dq ProcessBinary  ; b
-            dq ProcessChar    ; c
-            dq ProcessDecimal ; d
-            dq ProcessInvalid ; e
-            dq ProcessInvalid ; f
-            dq ProcessInvalid ; g
-            dq ProcessInvalid ; h
-            dq ProcessInvalid ; i
-            dq ProcessInvalid ; j
-            dq ProcessInvalid ; k
-            dq ProcessInvalid ; l
-            dq ProcessInvalid ; m
-            dq ProcessInvalid ; n
-            dq ProcessOct     ; o
-            dq ProcessInvalid ; p
-            dq ProcessInvalid ; q
-            dq ProcessInvalid ; r
-            dq ProcessString  ; s
-            dq ProcessInvalid ; t
-            dq ProcessInvalid ; u
-            dq ProcessInvalid ; v
-            dq ProcessInvalid ; w
-            dq ProcessHex     ; x
-            dq ProcessInvalid ; y
-            dq ProcessInvalid ; z
+            dq processInvalid ; a
+            dq processBinary  ; b
+            dq processChar    ; c
+            dq processDecimal ; d
+            dq processInvalid ; e
+            dq processInvalid ; f
+            dq processInvalid ; g
+            dq processInvalid ; h
+            dq processInvalid ; i
+            dq processInvalid ; j
+            dq processInvalid ; k
+            dq processInvalid ; l
+            dq processInvalid ; m
+            dq processInvalid ; n
+            dq processOct     ; o
+            dq processInvalid ; p
+            dq processInvalid ; q
+            dq processInvalid ; r
+            dq processString  ; s
+            dq processInvalid ; t
+            dq processInvalid ; u
+            dq processInvalid ; v
+            dq processInvalid ; w
+            dq processHex     ; x
+            dq processInvalid ; y
+            dq processInvalid ; z
